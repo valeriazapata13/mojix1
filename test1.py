6851979ea96bde863992d30255ba88fd16ccb924
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-13 09:31:01 -0400

    ninth commit

--- test1.py
+++ test1.py
@@ -1,9 +1,11 @@
 import streamlit as st
+from PIL import Image
 st.write("hello there!")
 color = st.color_picker('Pick A Color', '#00565D')
 st.write('The current color is', color)
 
 st.balloons()
+
 
 image = Image.open('image.jpg')
 st.image(image, caption='schnauzer mini')
