36a428897c1f1c7d0a080f5db04c57e2109c1347
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-13 09:16:35 -0400

    sixth

--- test1.py
+++ test1.py
@@ -3,7 +3,4 @@ st.write("hello there!")
 color = st.color_picker('Pick A Color', '#00565D')
 st.write('The current color is', color)
 
-d = st.date_input(
-     "When's your birthday",
-     datetime.date(2019, 7, 6))
-st.write('Your birthday is:', d)
+st.balloons()
