479c982db9eb8a983af8a30bbe8a22c81fe1feb1
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-13 09:21:43 -0400

    seventh commit

--- test1.py
+++ test1.py
@@ -4,3 +4,11 @@ color = st.color_picker('Pick A Color', '#00565D')
 st.write('The current color is', color)
 
 st.balloons()
+
+[theme]
+base="light"
+[theme]
+base="dark"
+primaryColor="purple"
+[theme]
+font="serif"
