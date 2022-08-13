2f8a64d0514b148d925757eb935eba5d0ecdc12b
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-13 09:13:35 -0400

    Fith commit

--- test1.py
+++ test1.py
@@ -1,5 +1,9 @@
 import streamlit as st
 st.write("hello there!")
-color = st.color_picker('Pick A Color', '#00f900')
+color = st.color_picker('Pick A Color', '#00565D')
 st.write('The current color is', color)
 
+d = st.date_input(
+     "When's your birthday",
+     datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
