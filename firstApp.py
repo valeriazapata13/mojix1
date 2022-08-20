0d9d08e1eae2d1725778a697283c48b32504d81f
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-20 17:59:12 -0400

    Commit5

--- firstApp.py
+++ firstApp.py
@@ -52,7 +52,7 @@ chart_data = pd.DataFrame(
 
 st.bar_chart(df_discrepancy.groupby(['Retail_Category']).sum()[['Unders']])
 
-st.bar_chart(df_discrepancy.groupby(['Retail_Product_Name']).sum()[['Retail_CCQTY']].sort_values('Retail_CCQTY')ascending=False)
+st.bar_chart(df_discrepancy.groupby(['Retail_Product_Name']).sum()[['Retail_CCQTY']].reset_index().sort_values('Retail_CCQTY',ascending=False))
 
 
 
