81b5de15e040fac3bfd20e3576561af05c51b98d
valeriazapata13 <valeriazapatahurtado@gmail.com>
2022-08-20 14:58:58 -0400

    Commit5

--- firstApp.py
+++ firstApp.py
@@ -1,3 +1,27 @@
-
+import pandasql as pandasql
 import pandas as pd
-from pandasql import sqldf
+#from pandasql import sqldf
+df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
+df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
+
+
+df_counted = df_counted.drop_duplicates("RFID")
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+
+my_cols_selected_A= ["Retail_Product_Color",
+"Retail_Product_Level1",
+"Retail_Product_Level1Name",
+"Retail_Product_Level2Name",
+"Retail_Product_Level3Name",
+"Retail_Product_Level4Name",
+"Retail_Product_Name",
+"Retail_Product_SKU",
+"Retail_Product_Size",
+"Retail_Product_Style",
+"Retail_SOHQTY"]
+
+df_A = df_expected[my_cols_selected_A]
+
+
+df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
+df_discrepancy.head()
